e5fac1b4769c21e88432ff1ba8cbffc3756b8b51
Adimac93 <Adimac93@gmail.com>
2021-11-26 18:36:37 +0100

    focus & hover sounds

---
+++ how-to-deploy-on-github-pages.md
@@ -0,0 +1,14 @@
+# How to deploy
+
+git init\
+git add .\
+git commit -m "initial commit"\
+git branch -M main\
+git remote add origin *git-url/*_project-name/_.git\
+git push -u origin main\
+
+vite.config.ts -> base: _/project-name/_\
+npm run build\
+git add dist -f\
+git commit -, "adding dist"\
+git subtree push --prefix dist origin gh-pages\
--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,29 @@
     
   </header>
   <main>
-    <div class="grid justify-items-center justify-center">
+    <div class="grid justify-items-center justify-center mx-10">
 
       <label class="font-bold pt-5" >Notes</label>
       <br>
-      <input type="text" class="note-content p-2 border-2 rounded-md " v-model="title" placeholder="Title...">
+      <input type="text" class="note-content p-2 border-2 rounded-md " v-model="title" placeholder="Title..." @focus="playSound('/mouse_click.mp3')">
       <br>
-      <textarea class="p-2 border-2 rounded-md" rows="5" cols="50" v-model="text" placeholder="Place for your note ↴"></textarea>
+      <textarea class="p-2 border-2 rounded-md" rows="5" cols="50" v-model="text" placeholder="Place for your note ↴" @focus="playSound('/mouse_click.mp3')"></textarea>
       <label class="p-2">{{text.length}} / {{maxChars}}</label>
       <div class="inline-flex">
-        <button v-if="text.length <= maxChars" class="note-save p-1 w-20 rounded-md bg-gray-100 hover:bg-gray-200 active:bg-gray-100 rounded-r-none" @click="saveNote()">Save</button>
+        <button v-if="text.length <= maxChars" class="note-save p-1 w-20 rounded-md bg-gray-100 hover:bg-gray-200 active:bg-gray-100 rounded-r-none" @click="saveNote(), playSound('/mouse_click.mp3')">Save</button>
         <button v-else class="note-save p-1 w-20 bg-gray-100 rounded-md opacity-70 rounded-r-none">Save</button>
-        <button class="note-save p-1 w-20 rounded-md bg-gray-100 hover:bg-gray-200 active:bg-gray-100 rounded-l-none" @click="clearFields()">Clear</button>
+        <button class="note-save p-1 w-20 rounded-md bg-gray-100 hover:bg-gray-200 active:bg-gray-100 rounded-l-none" @click="clearFields(), playSound('/mouse_click.mp3')">Clear</button>
       </div>
       
       <br>
-      <ul class="note-view  hover:opacity-50">
-        <li class="note" v-for="(note, index) in notes" :key="note.title" @click="displayNote(index)" @dblclick="delNote(index)">
+      <ul class="note-view items-center cursor-pointer">
+        <li class="note text-center break-words max-w-6xl hover:bg-gray-100 active:bg-gray-200 rounded-md" 
+        v-for="(note, index) in notes" :key="note.title" 
+        @click="displayNote(index)" 
+        @dblclick="delNote(index)"
+        @mouseover="playSound('slide.mp3')"
+        >
           <a class="font-bold">{{note.title}}</a> - {{note.text}}
-          
         </li>
         
       </ul>
@@ -36,26 +40,33 @@
 import { defineComponent } from '@vue/runtime-core'
 import {Note} from './Note'
 export default defineComponent({
+
   name: "app",
   data() {
     return {
+
       title:<string> '',
       text:<string> '',
       notes:<Note[]> JSON.parse(localStorage.getItem("notes") ?? "[]"),
 
-      maxChars:<number> 100,
+      maxChars:<number> 1000,
       currentChars:<number> 0,
 
-      
     }
   },
   methods: {
+    playSound(sound: string) {
+      if(sound) {
+        var audio = new Audio(sound);
+        audio.volume = 0.05;
+        audio.play();
+      }
+    },
     saveNote(): void {
       if (this.title != '' && this.text != '') {
         this.notes.unshift({
         title: this.title,
         text: this.text
-        
         
       })
       localStorage.setItem("notes", JSON.stringify(this.notes))
@@ -71,16 +82,11 @@ export default defineComponent({
       this.text = this.notes[itemIndex]["text"]
     },
     delNote(itemIndex: number) {
-      console.log(this.notes)
       this.notes.splice(itemIndex, 1)
-      console.log(this.notes)
       localStorage.setItem("notes", JSON.stringify(this.notes))
       }
     },
-    counterUpdate() {
-      this.currentChars = this.text.length
-      
-    },
+    
     
 });
 </script>
